7045eabee4a27cf36d146d07b5318cb5cba113b4
Donald Cameron <don@ddsc.us>
2022-07-02 12:33:23 -0500

    Category Item Component

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,26 @@
+/**
+ * Created by PHP Storm
+ * Author: Don Cameron
+ * File Type: React Component
+ * Date: 7/2/2022
+ * Time: 12:00 PM
+ */
+import "./category-item.styles.scss";
+
+const CategoryItem = ({ category }) => {
+  const { imageUrl, title } = category;
+  return (
+    <div className="category-container">
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
+      <div className="category-body-container">
+        <h2>{title}</h2>
+        <p>Shop Now</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryItem;
