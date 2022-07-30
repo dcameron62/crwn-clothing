05583ec85b8344ed29141fe02287320c0689790b
Donald Cameron <don@ddsc.us>
2022-07-30 09:31:23 -0500

    complete category page

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,35 @@
+/**
+ * Created by PHP Storm
+ * Author: Don Cameron
+ * File Type: React Component
+ * Date: 7/30/2022
+ * Time: 8:56 AM
+ */
+
+import {useParams} from "react-router-dom";
+import './category.styles.scss'
+import {useContext, useEffect, useState} from "react";
+import {CategoriesContext} from "../../contexts/categories.context";
+import ShopCard from "../../components/shop-card/shop-card.component";
+
+const Category = () => {
+    //make the use of parameters available. use params comes in as an object
+    const {category} = useParams();
+    //bring in date through context
+    const {categoriesMap} = useContext(CategoriesContext);
+    //use state to maintain categories
+    const [products, setProducts] = useState(categoriesMap[category])
+
+    //load products when page loads and when category or categoriesMap => state changes
+    useEffect(() => {
+        setProducts(categoriesMap[category])
+    }, [category, categoriesMap])
+    return (
+        <div className='category-container'>
+            {products &&
+                products.map((product) => <ShopCard key={product.id} shopData={product}/>)}
+        </div>
+    )
+}
+
+export default Category;
