1e961e6f9542c9d5be53082953e651514e9bda14
Donald Cameron <don@ddsc.us>
2022-08-11 18:33:11 -0500

    Revert "set up the store"
    This reverts commit 3f8d0f3ec492d511f331801d2550857aa0de3f05.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
+// import {render} from 'react-dom';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
 import reportWebVitals from "./reportWebVitals";
@@ -12,7 +11,7 @@ import { CartProvider } from "./contexts/cart.context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -22,7 +21,6 @@ root.render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
