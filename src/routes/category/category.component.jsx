3afbc265d0afea92c3edfc845f331382740cd00c
Donald Cameron <don@ddsc.us>
2022-08-12 21:09:51 -0500

    categories selectors

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -6,21 +6,22 @@
  * Time: 8:56 AM
  */
 
-import { useParams } from "react-router-dom";
-import { Fragment, useContext, useEffect, useState } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import {useParams} from "react-router-dom";
+import {Fragment, useEffect, useState} from "react";
 import ShopCard from "../../components/shop-card/shop-card.component";
 import {CategoryContainer, CategoryTitle} from "./category.styles";
+import {useSelector} from "react-redux";
+import {selectCategoriesMap} from "../../store/categories/category.selector";
 
 const Category = () => {
-  //make the use of parameters available. use params comes in as an object
-  const { category } = useParams();
-  //bring in date through context
-  const { categoriesMap } = useContext(CategoriesContext);
-  //use state to maintain categories
-  const [products, setProducts] = useState(categoriesMap[category]);
+    const categoriesMap = useSelector(selectCategoriesMap);
+    //make the use of parameters available. use params comes in as an object
+    const {category} = useParams();
+    //bring in date through context
+    //use state to maintain categories
+    const [products, setProducts] = useState(categoriesMap[category]);
 
-  //load products when page loads and when category or categoriesMap => state changes
+    //load products when page loads and when category or categoriesMap => state changes
   useEffect(() => {
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -8,14 +8,29 @@
 import {Routes, Route} from "react-router-dom";
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import {useEffect} from "react";
+import {useDispatch} from "react-redux";
+
+import {getCategoriesAndDocuments} from "../../utils/firebase/firebase.utils";
+import {setCategoriesMap} from "../../store/categories/category.action";
 
 const Shop = () => {
-  return (
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments("categories");
+      dispatch(setCategoriesMap(categoryMap));
+    };
+
+    getCategoriesMap();
+  }, []);
+
+  return (
       <Routes>
-    <Route index element={<CategoriesPreview />} />
-    <Route path=':category' element={<Category />} />
-  </Routes>
+        <Route index element={<CategoriesPreview/>}/>
+        <Route path=":category" element={<Category/>}/>
+      </Routes>
   );
 };
 
